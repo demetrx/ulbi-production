77abb5c3c2ceab3d216c89c1e124597d7c72994f
demetrxx <dmytro.bielousov@bndigital.co>
2023-02-17 22:13:47 +0200

    add i18next + babel with configs

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -10,6 +10,28 @@ export function buildLoaders({isDev}: BuildOptions): webpack.RuleSetRule[] {
     use: ['@svgr/webpack']
   }
 
+  const babelLoader = {
+    test: /\.(js|jsx|ts|tsx)$/,
+    exclude: /node_modules/,
+    use: {
+      loader: "babel-loader",
+      options: {
+        presets: ['@babel/preset-env'],
+        plugins: [
+          [
+            "i18next-extract",
+            {
+              locales: ["en", "ua"],
+              keyAsDefaultValue: false,
+              saveMissing: true,
+              outputPath: 'public/locales/{{locale}}/{{ns}}.json'
+            }
+          ]
+        ]
+      }
+    }
+  }
+
   const fileLoader = {
     test: /\.(png|jpe?g|gif|woff2|woff)$/i,
     use: [
@@ -46,6 +68,7 @@ export function buildLoaders({isDev}: BuildOptions): webpack.RuleSetRule[] {
   return [
     fileLoader,
     svgLoader,
+    babelLoader,
     typescriptLoader,
     cssLoader,
   ]
