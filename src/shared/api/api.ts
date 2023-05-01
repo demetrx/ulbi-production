35c1da98ba582f2bbd82a2e823cac3a2af9f5263
demetrxx <dmytro.bielousov@bndigital.co>
2023-05-01 09:45:10 +0300

    fix api config

--- src/shared/api/api.ts
+++ src/shared/api/api.ts
@@ -4,7 +4,13 @@ import { USER_LS_KEY } from 'shared/consts/localStorage';
 const baseURL = __API_URL__;
 export const $api = axios.create({
   baseURL,
-  headers: {
-    authorization: localStorage.getItem(USER_LS_KEY) || '',
-  },
+});
+
+$api.interceptors.request.use((config) => {
+  if (config.headers) {
+    // eslint-disable-next-line no-param-reassign
+    config.headers.Authorization = localStorage.getItem(USER_LS_KEY) || '';
+  }
+
+  return config;
 });
