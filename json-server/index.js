229ea415ded00c09a3be831d49c91be121774ac0
demetrxx <dmytro.bielousov@bndigital.co>
2023-05-23 16:39:41 +0300

    add nginx config default

--- json-server/index.js
+++ json-server/index.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const jsonServer = require('json-server');
 const path = require('path');
 const https = require('https');
+const http = require('https');
 
 const options = {
   key: fs.readFileSync(path.resolve(__dirname, 'key.pem')),
@@ -48,10 +49,16 @@ server.use(async (req, res, next) => {
 
 server.use(router);
 
+const httpServer = http.createServer(server);
 const httpsServer = https.createServer(options, server);
 
-const PORT = 8443;
+const HTTP_PORT = 8000;
+const HTTPS_PORT = 8443;
 
-httpsServer.listen(PORT, () => {
-  console.log(`Server is running on ${PORT} port`);
+httpServer.listen(HTTP_PORT, () => {
+  console.log(`HTTP server is running on ${HTTP_PORT} port`);
+});
+
+httpsServer.listen(HTTPS_PORT, () => {
+  console.log(`HTTPS Server is running on ${HTTPS_PORT} port`);
 });
