8f8ae4a2d6594e6d54a555d2a1d870f8a8aa732d
demetrxx <dmytro.bielousov@bndigital.co>
2023-06-10 17:14:11 +0300

    chore(logo): change app logo icon

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { Page } from '@/widgets/Page';
+import { VStack } from '@/shared/ui/redesigned/Stack';
+import { Text } from '@/shared/ui/redesigned/Text';
 
 const MainPage = () => {
     const { t } = useTranslation();
@@ -12,8 +14,11 @@ const MainPage = () => {
 
     return (
         <Page data-testid="MainPage">
-            <div>123123123123123123</div>
-            {t('Main page')}
+            <VStack max gap="16" align="center">
+                <Text size="l" align="center" title={t('Main page')} />
+                <Text text={t('Main info')} />
+                <Text text={t('Main credentials')} />
+            </VStack>
         </Page>
     );
 };
